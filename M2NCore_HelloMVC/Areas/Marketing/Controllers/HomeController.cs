c9fd951afa0115424d87e3ffc821d2be49b92676
Luis Chang <luis.chang@outlook.com>
2021-10-14 12:46:34 -0500

    Agregar archivos de proyecto.

---
+++ M2NCore_HelloMVC/Areas/Marketing/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M2NCore_HelloMVC.Areas.Marketing.Controllers
+{
+    [Area("Marketing")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ M2NCore_HelloMVC/Controllers/SecurityController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M2NCore_HelloMVC.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(IFormCollection form)
+        {
+            var email = form["email"].ToString();
+            var password = form["password"].ToString();
+
+            if (email == "luis@qboinstitute.com" && password == "123456")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return RedirectToAction("Login");
+
+        }
+
+
+    }
+}
